394659e70f19ac5e41fc420eb2838e83420dffee
mugicha755 <mugicha.755@gmail.com>
2018-11-30 18:06:03 +0900

    Add: Add project files

---
+++ CleanSample/CleanSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanSample
+//
+//  Created by u2008016 on 2018/11/30.
+//  Copyright © 2018 mugicha755. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
